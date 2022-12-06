3093b437f20772281f2bca7cbd5150b09c896fd9
keisuke murae <keisuke.murae@gmail.com>
2022-12-06 09:24:28 +0900

    P.106

--- src/src/p105.php
+++ src/src/p105.php
@@ -7,3 +7,11 @@ $t && print (1) . PHP_EOL; // 表示される
 $f && print (2) . PHP_EOL; // 表示ない
 $t || print (3) . PHP_EOL; // 表示ない
 $f || print (4) . PHP_EOL; // 表示される
+
+define('URL_BASE', 'http://www.example.com');
+
+defined('URL_BASE') || defined('URL_BASE', 'http://default.example.com');
+
+echo ('<pre>');
+var_dump(URL_BASE);
+echo ('</pre>');
